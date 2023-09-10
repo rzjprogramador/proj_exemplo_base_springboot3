794ac7bd8f7c1129e7a38030e9497608738f5509
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-09-10 19:46:57 -0300

    feat: inicio springboot api 3.0 em set/23

---
+++ src/main/java/com/reizao/proj_exemplo_base/ProjExemploBaseApplication.java
@@ -0,0 +1,13 @@
+package com.reizao.proj_exemplo_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjExemploBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjExemploBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reizao/proj_exemplo_base/ProjExemploBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reizao.proj_exemplo_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjExemploBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
